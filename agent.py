b938a2e59eebf9fd95f6e337fa5fdbac8c05d380
cholae <colekrajewski24@gmail.com>
2023-10-22 19:27:13 -0500

    group logic

--- agent.py
+++ agent.py
@@ -1,8 +1,9 @@
 from objects import Object;
 
-class Agent(Object):
+class Agent():
     def __init__(self, name, description, age, health, alignment):
-        super().__init__(name, description)
+        self.name = name
+        self.description = description
         self.age = age                 # e.g., 24
         self.health = health           # e.g., "healthy", "sick", "injured"
         self.alignment = alignment       # e.g., -1 to 1 from evil to good
@@ -33,24 +34,57 @@ class Agent(Object):
     def link_event(self, obj):
         self.event_memory[obj.name] = obj
     
-    def link_agent(self, agent):
+    def link_agent(self, agent, rr=0.0):
         self.agent_relations[agent.name] = {
                                             "relation": "neutral",
-                                            "relation_rating": 0.0,
+                                            "relation_rating": rr,
                                             "interactions": ["Initial Meeting"] 
                                            }
     
     def add_interaction(self, agent, interaction):
-        self.agent_relations[agent.name]["relation_rating"] += interaction.relation_impact
-        rr = self.agent_relations[agent.name]["relation_rating"]
-        if rr >= .5:
-            self.agent_relations[agent.name]["relation"] = "friends"
-        elif rr < .5 and rr >= 0:
-            self.agent_relations[agent.name]["relation"] = "neutral"
-        else:
-            self.agent_relations[agent.name]["relation"] = "enemies"
+        try:
+            print(agent.name + " is giving the rr")
+            print(self.name + " is recieving the rr")
+            self.agent_relations[agent.name]["relation_rating"] += interaction.relation_impact
+            rr = self.agent_relations[agent.name]["relation_rating"]
+            if rr >= .5:
+                self.agent_relations[agent.name]["relation"] = "friends"
+            elif rr < .5 and rr >= 0:
+                self.agent_relations[agent.name]["relation"] = "neutral"
+            else:
+                self.agent_relations[agent.name]["relation"] = "enemies"
 
-        self.agent_relations[agent.name]["interactions"].append(interaction.description)
+            self.agent_relations[agent.name]["interactions"].append(interaction.description)
+        except KeyError:
+            self.link_agent(agent, interaction.relation_impact)
+
+    #voting mechanism for groups
+    def vote(self, members):
+        #set vote to first member
+        curVoteMember = members[0]
+
+        #check if member is self
+        if(curVoteMember == self):
+            curVoteMember = members[1]
+
+        #check if member is a known relation
+        try:
+            self.agent_relations[curVoteMember.name]
+        except KeyError:
+            self.link_agent(curVoteMember)
+
+        #loop through group members, check relations for highest relation to current Agent
+        for member in members:
+            try:
+                curRel = self.agent_relations[member.name]
+            except:
+                curRel = None
+            if curRel: #if relationship exists check rating
+                if curRel["relation_rating"] > self.agent_relations[curVoteMember.name]["relation_rating"]:
+                    curVoteMember = member
+        curVoteMember.add_interaction(self, Interaction("Voted for me during an election", .05))
+        return curVoteMember
+
 
 class Interaction():
     def __init__(self, description, relation_impact):
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from environment import Environment;
 from objects import Object, Event, Resource, Effect;
 from agent import Agent, Interaction;
+from group import Group;
 
 try:
     env = Environment("Devland", 250, 100)
@@ -8,23 +9,39 @@ try:
     res2 = Resource("Lumber", "with plenty of water in the area and a limited population, there is plenty of forest here",.92, .94, .15)
     env.add_object(res1)
     env.add_object(res2)
-    print(env.describe())
 
     event1 = Event("Population Threshold 100", "Rise in population raised pollution levels in water", 0, "Population Increase", 0, -.05,0)
     env.add_object(event1)
     event1.apply_effect(res1, Effect(event1.impact,event1.description))
-    print(env.describe())
 
     player = Agent("Cole", "Dark brown hair, nearly 6 feet tall", 24, "healthy", .5)
-    print(player.describe())
+    
 
-    agent2 = Agent("Travis", "Short dark brown hair, loves chat gpt", 25, "healthy", .6)
-    player.link_agent(agent2)
+    agent2 = Agent("Travis", "Short dark brown hair, loves chat gpt", 25, "healthy", .5)
+    agent2.link_agent(player)
+    
+
+    agent2.add_interaction(player, Interaction("Beat him in fantasy football", -0.6))
+
+    agent3 = Agent("Matt", "UPS Driver. Father of Cole", 50, "healthy", .5)
+    agent2.link_agent(agent3)
+    player.link_agent(agent3, .5)
+
+    group = Group("Beeg Coalition")
+    group.add_member(player)
+    group.add_member(agent2)
+    group.add_member(agent3)
+    print("An election is taking place.")
+    group.elect_leader()
+
+    print("Cole has relations: ")
     print(player.agent_relations)
 
-    player.add_interaction(agent2, Interaction("Got lunch together", 0.6))
-    print(player.agent_relations)
+    print("Travis has relations:")
+    print(agent2.agent_relations)
 
+    print("Matt has relations:")
+    print(agent3.agent_relations)
 
 
 except NameError as err:
